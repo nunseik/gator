11fc271bc73e46cc444b70e3170a6960a7276561
Fabio Dantas Fernandes <nunseik@gmail.com>
2025-07-17 17:58:39 -0300

    handlerRegister basic logic

--- commands.go
+++ commands.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/nunseik/gator/internal/config"
 	"github.com/nunseik/gator/internal/database"
 )
 
 type state struct {
-	db *database.Queries
+	db     *database.Queries
 	config *config.Config
 }
 
@@ -40,4 +43,19 @@ func (c *commands) run(s *state, cmd command) error {
 
 func (c *commands) register(name string, f func(*state, command) error) {
 	c.commands[name] = f
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.commands) == 0 {
+		return errors.New("register command requires a username")
+	}
+	username := cmd.commands[0]
+	// Generate a unique ID for the user using UUID into int32 somehow, not working with int32 directly
+	userUniqueID := uuid.New()
+	newUser, err := s.db.CreateUser(context.Background(), database.CreateUserParams{ID: userUniqueID, CreatedAt: time.Now(), UpdatedAt: time.Now(), Name: username})
+	if err != nil {
+		return fmt.Errorf("could not create user: %v", err)
+	}
+	s.config.CurrentUserName = newUser.Name
+	return nil
 }
